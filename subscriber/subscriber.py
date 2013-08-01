75228203db52887ff3a890f039e242c0548a0b09
Andrei Mateescu <andrei@amateescu.me>
2013-08-01 14:54:30 +0200

    Added the zeromq python subscriber that will run on the RPi.

---
+++ subscriber/subscriber.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+import sys
+import zmq
+
+gyro = os.path.join(os.path.abspath(sys.path[0]), '../gyro/gyro')
+textToSpeech = os.path.join(os.path.abspath(sys.path[0]), '../text-to-speech/text-to-speech.py')
+
+context = zmq.Context()
+sock = context.socket(zmq.SUB)
+sock.setsockopt(zmq.SUBSCRIBE, '')
+
+sock.connect(sys.argv[1])
+
+while True:
+    message = sock.recv()
+    subprocess.check_call([ gyro, '5' ])
+    subprocess.check_call([ 'python', textToSpeech, 'en', message ])
